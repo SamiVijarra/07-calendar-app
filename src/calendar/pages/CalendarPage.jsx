4bffbb2384b860e564db42de360602487f34abc6
SamiVijarra <samivijarra1@gmail.com>
2026-03-23 21:53:38 -0300

    feat: add events on calendar events

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -2,8 +2,9 @@ import { Calendar } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 import { addHours } from 'date-fns';
-import { Navbar } from "../";
+import { Navbar, CalendarEvent } from "../";
 import { getMessagesES, localizer } from '../../helpers'
+import { useState } from 'react';
 
 
 
@@ -21,8 +22,10 @@ const events = [{
 
 export const CalendarPage = () => {
 
+    const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'week' )
+
     const eventStyleGetter = (event, start, end, isSelected ) => {
-        console.log({ event, start, end, isSelected });
+
         const style = {
             backgroundColor: '#347CF7',
             borderRadius: '0px',
@@ -33,7 +36,20 @@ export const CalendarPage = () => {
             style
         }
     }
-    
+
+    const onDoubleClick = (event) => {
+        console.log({ onDoubleClick: event });
+    }; 
+
+    const onSelect = (event) => {
+        console.log({ click: event });
+    }; 
+
+    const onViewChanged = (event) => {
+        localStorage.setItem('lastView', event);
+        setLastView(event)
+    }
+
     return (
         <>
             <Navbar />
@@ -42,11 +58,18 @@ export const CalendarPage = () => {
                 culture= 'es'
                 localizer={localizer}
                 events={events}
+                defaultView={lastView}
                 startAccessor="start"
                 endAccessor="end"
                 style={{ height: 'calc(100vh - 80px)' }}
                 messages={getMessagesES()}
-                eventPropGetter={ eventStyleGetter }
+                eventPropGetter={eventStyleGetter}
+                components={{
+                    event: CalendarEvent
+                }}
+                onDoubleClickEvent={onDoubleClick}
+                onSelectEvent={onSelect}
+                onView= {onViewChanged}
             />
         </>
     )
