6802047ed9bbc3cd02d399a92cc6e398c3450ec4
Sami <samivijarra1@gmail.com>
2026-04-11 19:34:58 -0300

    fix: resolve name on index barrel

--- src/calendar/index.js
+++ src/calendar/index.js
@@ -1,6 +1,6 @@
 export * from './components/CalendarEvent';
 export * from './components/CalendarModal';
-export * from './components/FabAddNew';
+export * from './components/fabAddNew';
 export * from './components/FabDelete';
 export * from './components/Navbar';
 
