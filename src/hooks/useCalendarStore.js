c60db8ab1018e5287229baf2942447290d291c49
Sami <samivijarra1@gmail.com>
2026-04-15 21:26:30 -0300

    feat: add start saving events

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,11 +1,13 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from '../store';
+import calendarApi from '../api/calendarApi';
 
 
 export const useCalendarStore = () => {
 
     const dispatch = useDispatch();
     const { events, activeEvent } = useSelector( state => state.calendar );
+    const { user } = useSelector( state => state.auth );
 
     const setActiveEvent = ( calendarEvent ) => {
         dispatch( onSetActiveEvent( calendarEvent ) )
@@ -19,8 +21,9 @@ export const useCalendarStore = () => {
             // Actualizando
             dispatch( onUpdateEvent({ ...calendarEvent }) );
         } else {
-            // Creando
-            dispatch( onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() }) );
+            const { data } = await calendarApi.post('/events', calendarEvent);
+
+            dispatch( onAddNewEvent({ ...calendarEvent, id: data.evento.id, user }) );
         }
     }
 
