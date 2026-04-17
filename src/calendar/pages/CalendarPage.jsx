24f21baa825a9a4e9b1d7fa3491c3e6c312e4ed8
Sami <samivijarra1@gmail.com>
2026-04-16 23:18:28 -0300

    style: apply ESLint rules across project

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -5,22 +5,24 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { Navbar, CalendarEvent, CalendarModal, FabAddNew, FabDelete} from '../';
 
 import { localizer, getMessagesES } from '../../helpers';
-import { useUiStore, useCalendarStore } from '../../hooks';
+import { useUiStore, useCalendarStore, useAuthStore } from '../../hooks';
 
 
 
 export const CalendarPage = () => {
     
-    
+    const { user } = useAuthStore();
     const { openDateModal } = useUiStore();
     const { events, setActiveEvent, startLoadingEvents } = useCalendarStore();
 
     const [ lastView, setLastView ] = useState(localStorage.getItem('lastView') || 'week' );
 
     const eventStyleGetter = ( event, start, end, isSelected ) => {
-
-    const style = {
-        backgroundColor: '#347CF7',
+        
+        const isMyEvent = ( user.uid === event.user._id ) || ( user.uid === event.user.uid );
+        
+        const style = {
+        backgroundColor: isMyEvent ? '#347CF7' : '#465660',
         borderRadius: '0px',
         opacity: 0.8,
         color: 'white'
