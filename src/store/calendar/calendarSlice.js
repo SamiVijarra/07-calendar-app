6a3e19a0c956adfcaf07dc66d72e7abc873fc04a
Sami <samivijarra1@gmail.com>
2026-04-16 23:10:39 -0300

    feat: add start saving event

--- src/store/calendar/calendarSlice.js
+++ src/store/calendar/calendarSlice.js
@@ -19,7 +19,7 @@ export const calendarSlice = createSlice({
         },
         onUpdateEvent: ( state, { payload } ) => {
             state.events = state.events.map( event => {
-                if ( event._id === payload._id ) {
+                if ( event.id === payload.id ) {
                     return payload;
                 }
 
@@ -28,14 +28,14 @@ export const calendarSlice = createSlice({
         },
         onDeleteEvent: ( state ) => {
             if ( state.activeEvent ) {
-                state.events = state.events.filter( event => event._id !== state.activeEvent._id );
+                state.events = state.events.filter( event => event.id !== state.activeEvent.id );
                 state.activeEvent = null;
             }
         },
         onLoadEvents: ( state, { payload = [] } ) => {
             state.isLoadingEvents = false;
             payload.forEach( event => {
-                const exists = state.events.some( dbEvent => dbEvent._id === event._id );
+                const exists = state.events.some( dbEvent => dbEvent.id === event.id );
                 if ( !exists ) {
                     state.events.push( event );
                 }
